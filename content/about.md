bce38956482060d59fb0bb2a428b18127c9fcf57
Gabri Botha <Angelodeath@outlook.com>
2020-09-23 23:11:44 +0200

    Commit code that was on HDD for a long time

---
+++ content/about.md
@@ -0,0 +1,6 @@
+---
+title: "About"
+date: 2020-07-06T13:20:17+02:00
+draft: true
+---
+
---
+++ serve.sh
@@ -0,0 +1,2 @@
+#! /usr/bin/env bash
+hugo serve --bind 0.0.0.0 --verbose --baseURL http://192.168.43.166:1313/
